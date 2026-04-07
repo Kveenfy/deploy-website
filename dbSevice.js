a2002aff024f76f3ef228a6c94ec095f2f692eec
Nikita <nikitafedorovich00@gmail.com>
2026-04-07 12:48:04 +0300

    изменён .env

--- dbSevice.js
+++ dbSevice.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.DB_PORT
